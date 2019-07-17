f7f481a149abccb71bd240e4f4d0893e953662c3
Johannes Ballé <jballe@google.com>
2019-07-17 13:06:16 -0700

    Updates pip dependencies.
    PiperOrigin-RevId: 258626531
    Change-Id: Ib3d2257bf71ef83822cbb734161e9cbf67bfee58

--- build_pip_pkg.py
+++ build_pip_pkg.py
@@ -67,8 +67,7 @@ def main(srcdir):
       # Contained modules and scripts.
       packages=setuptools.find_packages(),
       install_requires=[
-          'scipy >= 1.0.0',
-          'tensorflow >= 1.14.0',
+          'scipy >= 1',
       ],
       script_args=['sdist', 'bdist_wheel'],
       # Add in any packaged data.
@@ -83,7 +82,11 @@ def main(srcdir):
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Topic :: Scientific/Engineering :: Mathematics',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Software Development :: Libraries',
