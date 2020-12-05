8b93a6ca810e8593b6f7389c0209bd86679c5da8
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-12-05 21:54:31 +1100

    [models/hific/helpers.py] Restore original version

--- models/hific/helpers.py
+++ models/hific/helpers.py
@@ -85,6 +85,6 @@ def add_tfds_arguments(parser):
       help="Name of the TFDS feature to use.")
 
 
-def parse_tfds_arguments(args) -> TFDSArgs:
+def parse_tfds_arguments(args) -> TFDSArguments:
   return TFDSArguments(args.tfds_dataset_name, args.tfds_features_key,
                        args.tfds_downloads_dir)
