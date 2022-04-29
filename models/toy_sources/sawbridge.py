8cd7152c6450cd63bcfd9950b30cfa0341077337
Sourbh Bhadane <eccentricph@gmail.com>
2022-04-29 15:40:07 -0400

    Sawbridge modified and pylint verified

--- models/toy_sources/sawbridge.py
+++ models/toy_sources/sawbridge.py
@@ -5,11 +5,13 @@ import tensorflow_probability as tfp
 
 
 class Sawbridge(tfp.distributions.Distribution):
-  """The "sawbridge": B(t) = t - 1(t > Z), where Z is uniform over [0,1]."""
+  """The "sawbridge": B(t) = t - 1(t > Z), where Z is uniform over [0,1].
+  The stationary sawbridge is B(t+V mod 1) where V is uniform over [0,1]
+  and Z,V are independent."""
 
-  def __init__(self, index_points, stationary=True, order=1,
-               dtype=tf.float32, validate_args=False, allow_nan_stats=True,
-               name="sawbridge"):
+  def __init__(self, index_points, phase=None, drop = None, stationary=True,
+               order=1,dtype=tf.float32, validate_args=False,
+               allow_nan_stats=True,name="sawbridge"):
     """Initializer.
 
     Args:
@@ -17,6 +19,8 @@ class Sawbridge(tfp.distributions.Distribution):
         the process. The intent is that all locations are in [0,1], but the
         process has a natural extrapolation outside this range so no error is
         thrown.
+      phase: Float in [0,1]. Specifies realization of V.
+      drop: Float in [0,1]. Specifies realization of Z.
       stationary: Boolean. Whether or not to "scramble" phase.
       order: Integer >= 1. The resulting process is a linear combination of
         `order` sawbridges.
@@ -32,6 +36,8 @@ class Sawbridge(tfp.distributions.Distribution):
           index_points, dtype_hint=dtype, name="index_points")
       self._stationary = bool(stationary)
       self._order = int(order)
+      self._phase = phase
+      self._drop = drop
     super().__init__(
         dtype=dtype,
         reparameterization_type=tfp.distributions.NOT_REPARAMETERIZED,
@@ -53,6 +59,14 @@ class Sawbridge(tfp.distributions.Distribution):
   def order(self):
     return self._order
 
+  @property
+  def phase(self):
+    return self._phase
+
+  @property
+  def drop(self):
+    return self._drop
+
   def _batch_shape_tensor(self):
     return tf.constant([], dtype=tf.int32)
 
@@ -66,13 +80,22 @@ class Sawbridge(tfp.distributions.Distribution):
     return self.index_points.shape
 
   def _sample_n(self, n, seed=None):
-    uniform = tf.random.uniform((self.order, n), seed=seed, dtype=self.dtype)
     ind = self.index_points
+    if self._drop is None:
+      uniform = tf.random.uniform((self.order,n),seed=seed,dtype = self.dtype)
+    else:
+      uniform = tf.repeat(self._drop,repeats = n*self.order)
+      uniform = tf.reshape(uniform,[self.order,n])
     # ind shape: (time)
     uniform = tf.expand_dims(uniform, -1)
     # uniform shape: (order, n, 1)
     if self.stationary:
-      ind += tf.random.uniform((n, 1), seed=seed, dtype=self.dtype)
+      if self._phase is None:
+        phase = tf.random.uniform((n, 1), seed=seed, dtype=self.dtype)
+      else:
+        phase = tf.repeat(self._phase,repeats=n,axis=0)
+        phase = tf.reshape(phase, [n,1])
+      ind += phase
       ind %= 1.
     less = tf.less(uniform, ind)
     # less shape: (order, n, time)
--- models/toy_sources/sinusoid.py
+++ models/toy_sources/sinusoid.py
@@ -1,61 +1,69 @@
+"""Sinusoid process."""
+
 import tensorflow as tf
 import numpy as np
 import tensorflow_probability as tfp
 
+
 class Sinusoid(tfp.distributions.Distribution):
-    """The "sinusoid": P(t) = sin(2pi(t+V)), where V is uniform over [0,1]"""
-    def __init__(self,index_points,phase=None,dtype=tf.float32,validate_args=False,allow_nan_stats=True,name='ramp'):
-    """
+  """The "sinusoid": P(t) = sin(2pi(t+V)), where V is uniform over [0,1]."""
+
+  def __init__(self, index_points, phase=None, dtype=tf.float32,
+               validate_args=False, allow_nan_stats=True, name="sinusoid"):
+    """Initializer.
+
     Args:
-      index_points: 1-D `Tensor` representing the locations at which to evaluate
-        the process. The intent is that all locations are in [0,1], but the
-        process has a natural extrapolation outside this range so no error is
-        thrown.
-      phase: Float in [0,1]. Specifies realization of V. 
-      dtype: Data type of the returned realization at each timestep. Defaults to
-        tf.float32.
+      index_points: 1-D `Tensor` representing the locations at which to
+        evaluate the process. The intent is that all locations are in [0,1],
+        but the process has a natural extrapolation outside this range so no
+        error is thrown.
+      phase: Float in [0,1]. Specifies realization of V.
+      dtype: Data type of the returned realization at each timestep. Defaults
+        to tf.float32.
       validate_args: required by tensorflow Distribution class but unused.
       allow_nan_stats: required by tensorflow Distribution class but unused.
       name: String. Name of the created object.
     """
- 
-        parameters = dict(locals())
-        with tf.name_scope(name) as name:
-            self._index_points = tf.convert_to_tensor(index_points,dtype_hint= dtype,name='index_points')
-            self._phase = phase
-        super().__init__(
-                dtype=dtype,
-                reparameterization_type = tfp.distributions.NOT_REPARAMETERIZED,
-                validate_args = validate_args,
-                allow_nan_stats = allow_nan_stats,
-                parameters = parameters,
-                name=name,)
-    @property
-    def index_points(self):
-        return self._index_points
+    parameters = dict(locals())
+    with tf.name_scope(name) as name:
+      self._index_points = tf.convert_to_tensor(index_points,dtype_hint= dtype,
+                                              name="index_points")
+      self._phase = phase
+    super().__init__(
+      dtype=dtype,
+      reparameterization_type = tfp.distributions.NOT_REPARAMETERIZED,
+      validate_args = validate_args,
+      allow_nan_stats = allow_nan_stats,
+      parameters = parameters,
+      name=name,)
 
-    @property 
-    def phase(self):
-        return self._phase
+  @property
+  def index_points(self):
+    return self._index_points
 
-    def _batch_shape_tensor(self):
-        return tf.constant([],dtype=tf.int32)
+  @property
+  def phase(self):
+    return self._phase
 
-    def _batch_shape(self):
-        return tf.TensorShape([])
+  def _batch_shape_tensor(self):
+    return tf.constant([],dtype=tf.int32)
 
-    def _event_shape(self):
-        return self.index_points.shape
+  def _batch_shape(self):
+    return tf.TensorShape([])
 
-    def _event_shape_tensor(self):
-        return tf.shape(self.index_points)
+  def _event_shape(self):
+    return self.index_points.shape
 
-    def _sample_n(self,n,seed=None):
-        ind = self.index_points
-        if self._phase is None:
-            ind = tf.sin(2*np.pi*(ind+tf.random.uniform((n,1),seed=seed,dtype=self.dtype)))
-        else:
-            phase = tf.repeat(self._phase,repeats=n,axis=0)
-            phase = tf.reshape(phase,[n,1])
-            ind = tf.sin(2*np.pi(ind+phase)) 
-        return ind
+  def _event_shape_tensor(self):
+    return tf.shape(self.index_points)
+
+  def _sample_n(self,n,seed=None):
+    ind = self.index_points
+    if self.phase is None:
+      ind = tf.sin(2*np.pi*(ind+tf.random.uniform((n,1),seed=seed,
+                                                  dtype=self.dtype)))
+    else:
+      phase = tf.repeat(self._phase,repeats=n,axis=0)
+      phase = tf.reshape(phase,[n,1])
+      ind = tf.sin(2*np.pi(ind+phase))
+      return ind
