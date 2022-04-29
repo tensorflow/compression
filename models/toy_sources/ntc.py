6d1f106c70f7dc0480b866bc714a6aa6a83c8253
Johannes Ballé <jballe@google.com>
2022-04-29 16:32:33 -0700

    Adds missing copyright notices.
    PiperOrigin-RevId: 445531928
    Change-Id: Ia00efca37d927141b2969105a1dea72cec64cd60

--- models/toy_sources/ntc.py
+++ models/toy_sources/ntc.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 """Nonlinear transform coding."""
 
 import matplotlib.pyplot as plt
@@ -8,11 +22,6 @@ import tensorflow_probability as tfp
 from toy_sources import compression_model
 
 tfpd = tfp.distributions
-
-
-@tf.custom_gradient
-def st_round(inputs):
-  return tf.round(inputs), lambda dy: dy
 
 
 class NTCModel(compression_model.CompressionModel):
@@ -158,7 +167,7 @@ class NTCModel(compression_model.CompressionModel):
       else:
         if guess_offset:
           offset += tfc.quantization_offset(prior)
-        return st_round(inputs - offset) + offset
+        return tfc.round_st(inputs, offset)
 
     assert x.shape[-1] == self.ndim_source
     y = self.analysis(x)
